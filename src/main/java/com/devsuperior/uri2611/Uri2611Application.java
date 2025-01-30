5e3e88e2ec83b5bbbce68bea5919abb5b1d566e8
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-01-30 16:48:30 -0300

    Usando SQL raiz

--- src/main/java/com/devsuperior/uri2611/Uri2611Application.java
+++ src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2611;
 
+import com.devsuperior.uri2611.dto.MovieMinDTO;
 import com.devsuperior.uri2611.projections.MovieMinProjection;
 import com.devsuperior.uri2611.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2611Application implements CommandLineRunner {
@@ -24,7 +26,13 @@ public class Uri2611Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		List<MovieMinProjection> list = repository.search1("Action");
+		List<MovieMinDTO> result = list.stream().map(x-> new MovieMinDTO(x)).collect(Collectors.toList());
 
+		System.out.println("\n*** RESULTADO SQL RAIZ");
+		for (MovieMinDTO obj : result){
+			System.out.println(obj);
+		}
+		System.out.println("\n\n");
 
 
 	}
