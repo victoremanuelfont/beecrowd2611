fd34610c0712f4c77ca7a16a3a94e66bcb7b50dc
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-01-30 16:30:07 -0300

    Para aparecer no console

--- src/main/java/com/devsuperior/uri2611/Uri2611Application.java
+++ src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -1,10 +1,13 @@
 package com.devsuperior.uri2611;
 
+import com.devsuperior.uri2611.projections.MovieMinProjection;
 import com.devsuperior.uri2611.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Uri2611Application implements CommandLineRunner {
@@ -19,6 +22,10 @@ public class Uri2611Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		List<MovieMinProjection> list = repository.search1("Action");
+		
+
 
 	}
 }
